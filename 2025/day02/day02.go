9efcbcc3a1ff85cf59d926a05439f0a425d70f3b
Alessandro Fascini <alessandro.fascini@farnetworks.com>
2025-12-02 09:03:53 +0100

    prep test

--- 2025/day02/day02.go
+++ 2025/day02/day02.go
@@ -1,1 +1,10 @@
 package day02
+
+type Range struct {
+	Lower int64
+	Upper int64
+}
+
+func Part1(rages []Range) int64 {
+	return 0
+}
--- 2025/day02/day02_test.go
+++ 2025/day02/day02_test.go
@@ -1,1 +1,14 @@
 package day02
+
+import "testing"
+
+func readInput(fileName string) []Range {
+	return make([]Range, 0)
+}
+
+func TestPart1(t *testing.T) {
+	input := readInput("test.txt")
+	t.Log("Test: ", Part1(input))
+	input = readInput("puzzle.txt")
+	t.Log("Puzzle: ", Part1(input))
+}
