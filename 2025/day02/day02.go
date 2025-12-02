e915b001970b984e2f8e1ea1f8d3146e4f81a108
Alessandro Fascini <alessandro.fascini@farnetworks.com>
2025-12-02 09:49:06 +0100

    day02 completed

--- 2025/day02/day02.go
+++ 2025/day02/day02.go
@@ -1,10 +1,87 @@
 package day02
 
+import (
+	"strconv"
+)
+
 type Range struct {
 	Lower int64
 	Upper int64
 }
 
-func Part1(rages []Range) int64 {
-	return 0
+func SplitEvery(s string, m int) []string {
+    if m <= 0 {
+        return []string{s}
+    }
+
+    var chunks []string
+    for i := 0; i < len(s); i += m {
+        end := i + m
+        if end > len(s) {
+            end = len(s)
+        }
+        chunks = append(chunks, s[i:end])
+    }
+    return chunks
+}
+
+func isInvalid(n int64) bool {
+	s := strconv.FormatInt(n, 10)
+	l := len(s) >> 1
+	return s[l:] == s[:l]
+}
+
+func invalidIds(r Range, isInvalid func (int64) bool) int64 {
+	ans := int64(0)
+	for i := r.Lower; i <= r.Upper; i++ {
+		if(isInvalid(i)) {
+			ans += i
+		}
+	}
+	return ans
+}
+
+func Part1(ranges []Range) int64 {
+	ans := int64(0)
+	for _, r := range ranges {
+		ans += invalidIds(r, isInvalid)
+	}
+	return ans
+}
+
+func isInvalid2(n int64) bool {
+	s := strconv.FormatInt(n, 10)
+	l := len(s) >> 1 + 1
+
+	check := func (s []string) bool {
+		if(len(s) < 2) {
+			return false
+		}
+		t := s[0]
+		for _, v := range s {
+			if v != t {
+				return false
+			}
+		}
+		return true
+	}
+
+	for i := l; i > 0; i-- {
+		if(len(s) % i != 0) {
+			continue
+		}
+		splitted := SplitEvery(s, i)
+		if(check(splitted)) {
+			return true
+		}
+	}
+	return false
+}
+
+func Part2(ranges []Range) int64 {
+	ans := int64(0)
+	for _, r := range ranges {
+		ans += invalidIds(r, isInvalid2)
+	}
+	return ans
 }
--- 2025/day02/day02_test.go
+++ 2025/day02/day02_test.go
@@ -1,14 +1,44 @@
 package day02
 
-import "testing"
+import (
+	"bufio"
+	"bytes"
+	"os"
+	"strconv"
+	"strings"
+	"testing"
+)
 
-func readInput(fileName string) []Range {
-	return make([]Range, 0)
+func readInput(t *testing.T, filename string) []Range {
+		b, err := os.ReadFile(filename)
+	if err != nil {
+		t.Fatal(err)
+	}
+	scanner := bufio.NewScanner(bytes.NewBuffer(b))
+	input := make([]Range, 0)
+	for scanner.Scan() {
+		line := scanner.Text()
+		ranges := strings.Split(line, ",")
+		for _, r := range ranges {
+			splitted := strings.Split(r, "-")
+			lower, _ := strconv.ParseInt(splitted[0], 10, 64)
+			upper, _ := strconv.ParseInt(splitted[1], 10, 64)
+			input = append(input, Range{lower, upper})
+		}
+	}
+	return input
 }
 
 func TestPart1(t *testing.T) {
-	input := readInput("test.txt")
+	input := readInput(t, "test.txt")
 	t.Log("Test: ", Part1(input))
-	input = readInput("puzzle.txt")
+	input = readInput(t, "puzzle.txt")
 	t.Log("Puzzle: ", Part1(input))
+}
+
+func TestPart2(t *testing.T) {
+	input := readInput(t, "test.txt")
+	t.Log("Test: ", Part2(input))
+	input = readInput(t, "puzzle.txt")
+	t.Log("Puzzle: ", Part2(input))
 }
