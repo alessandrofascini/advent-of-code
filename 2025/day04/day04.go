887330c3a2fefe5935ce1b6a26abb977748fdd25
Alessandro Fascini <alessandro.fascini@farnetworks.com>
2025-12-04 09:28:08 +0100

    part 1

--- 2025/day04/day04.go
+++ 2025/day04/day04.go
@@ -1,1 +1,45 @@
 package day04
+
+const NOTHING = '.'
+const ROOL_OF_PAPER = '@'
+
+type Row = string
+
+var directions = [][]int{
+	{-1,-1},
+	{-1, 0},
+	{-1, 1},
+	{ 0,-1},
+	{ 0, 1},
+	{ 1,-1},
+	{ 1, 0},
+	{ 1, 1},
+}
+
+func canAccess(i, j int, grid []Row) int {
+	n, m := len(grid), len(grid[0])
+	count := 0
+	for _, dir := range directions {
+		ni, nj := i+dir[0], j+dir[1]
+		if 0 <= ni && ni < n && 0 <= nj && nj < m && grid[ni][nj] == ROOL_OF_PAPER {
+			count++
+		}
+	}
+
+	return count
+}
+
+
+func Part1(grid []Row) int64 {
+	ans := int64(0)
+
+	for i, row := range grid {
+		for j, cell := range row {
+			if cell == ROOL_OF_PAPER && canAccess(i, j, grid) < 4 {
+				ans++
+			}	
+		}
+	}
+
+	return ans
+}
--- 2025/day04/day04_test.go
+++ 2025/day04/day04_test.go
@@ -1,1 +1,30 @@
 package day04
+
+import (
+	"bufio"
+	"bytes"
+	"os"
+	"testing"
+)
+
+func ReadInput(t *testing.T, filename string) []Row {
+	b, err := os.ReadFile(filename)
+	if err != nil {
+		t.Fatal(err)
+	}
+	scanner := bufio.NewScanner(bytes.NewBuffer(b))
+	input := make([]Row, 0)
+	for scanner.Scan() {
+		line := scanner.Text()
+		input = append(input, line)
+	}
+	return input
+}
+
+func TestPart1(t *testing.T) {
+	input := ReadInput(t, "test.txt")
+	t.Log("test:", Part1(input))
+
+	input = ReadInput(t, "puzzle.txt")
+	t.Log("puzzle:", Part1(input))
+}
