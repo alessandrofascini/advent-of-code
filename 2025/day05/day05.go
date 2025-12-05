9c5a8a00df84a1653f096d1ddf0a994c735eb3da
Alessandro Fascini <alessandrofascini@proton.me>
2025-12-05 12:17:50 +0100

    completed part1 and part2

--- 2025/day05/day05.go
+++ 2025/day05/day05.go
@@ -1,9 +1,82 @@
 package day05
 
+import "sort"
+
 type IngredientId = int
 
-type Range = [2]int
+type Interval [2]int
 
-func Part1(r []Range, ingredients []IngredientId) int {
-	return 0
+func NewInterval(start, end int) Interval {
+	if start > end {
+		return Interval{end, start}
+	}
+	return Interval{start, end}
+}
+
+func Merge(a, b Interval) Interval {
+	return NewInterval(min(a[0], b[0]), max(a[1], b[1]))
+}
+
+func (i Interval) Compare(other Interval) int {
+	a, b := i[0], i[1]
+	c, d := other[0], other[1]
+	switch {
+	case d < a:
+		return -1
+	case b < c:
+		return 1
+	default:
+		return 0
+	}
+}
+
+func (i Interval) IsInside(v int) bool {
+	return i[0] <= v && v <= i[1]
+}
+
+func belongs(ranges []Interval, id IngredientId) bool {
+	for _, interval := range ranges {
+		if interval.IsInside(id) {
+			return true
+		}
+	}
+	return false
+}
+
+func Part1(ranges []Interval, ingredients []IngredientId) int {
+	ans := 0
+	for _, ingredient := range ingredients {
+		if belongs(ranges, ingredient) {
+			ans++
+		}
+	}
+	return ans
+}
+
+func Part2(ranges []Interval) int {
+	// merge ranges
+	sort.Slice(ranges, func(i, j int) bool {
+		return ranges[i].Compare(ranges[j]) > 0
+	})
+
+	newRanges := make([]Interval, 0, len(ranges))
+	for _, interval := range ranges {
+		i := len(newRanges) - 1
+		if i == -1 {
+			newRanges = append(newRanges, interval)
+			continue
+		}
+		curr := interval
+		for i > -1 && newRanges[i].Compare(curr) == 0 {
+			curr = Merge(newRanges[i], curr)
+			i--
+		}
+		newRanges = append(newRanges[:i+1], curr)
+	}
+
+	ans := 0
+	for _, interval := range newRanges {
+		ans += interval[1] - interval[0] + 1
+	}
+	return ans
 }
--- 2025/day05/day05_test.go
+++ 2025/day05/day05_test.go
@@ -9,13 +9,32 @@ import (
 	"testing"
 )
 
-func ReadInput(t *testing.T, filename string) ([]Range, []IngredientId) {
+func TestInterval_Compare(t *testing.T) {
+	base := NewInterval(5, 10)
+	compares := [][]int{
+		{base.Compare(NewInterval(2, 3)), -1},
+		{base.Compare(NewInterval(2, 6)), 0},
+		{base.Compare(NewInterval(6, 8)), 0},
+		{base.Compare(NewInterval(8, 11)), 0},
+		{base.Compare(NewInterval(11, 12)), 1},
+	}
+
+	for _, compare := range compares {
+		value, expected := compare[0], compare[1]
+		if value != expected {
+			t.Error("Expected", expected, "Got", value)
+		}
+	}
+
+}
+
+func ReadInput(t *testing.T, filename string) ([]Interval, []IngredientId) {
 	b, err := os.ReadFile(filename)
 	if err != nil {
 		t.Fatal(err)
 	}
 	scanner := bufio.NewScanner(bytes.NewBuffer(b))
-	ranges := make([]Range, 0)
+	ranges := make([]Interval, 0)
 
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -25,7 +44,7 @@ func ReadInput(t *testing.T, filename string) ([]Range, []IngredientId) {
 		split := strings.Split(line, "-")
 		lower, _ := strconv.Atoi(split[0])
 		upper, _ := strconv.Atoi(split[1])
-		ranges = append(ranges, Range{lower, upper})
+		ranges = append(ranges, NewInterval(lower, upper))
 	}
 
 	ingredients := make([]IngredientId, 0)
@@ -41,4 +60,15 @@ func ReadInput(t *testing.T, filename string) ([]Range, []IngredientId) {
 func TestPart1(t *testing.T) {
 	ranges, ingredients := ReadInput(t, "test.txt")
 	t.Log("Test Part 1:", Part1(ranges, ingredients))
+
+	ranges, ingredients = ReadInput(t, "puzzle.txt")
+	t.Log("Puzzle Part 1:", Part1(ranges, ingredients))
+}
+
+func TestPart2(t *testing.T) {
+	ranges, _ := ReadInput(t, "test.txt")
+	t.Log("Test Part 2:", Part2(ranges))
+
+	ranges, _ = ReadInput(t, "puzzle.txt")
+	t.Log("Puzzle Part 2:", Part2(ranges))
 }
